d8d70b548212740b59995f9ded729492bab3747b
christy8m <christy8m@gmail.com>
2025-02-05 16:48:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-import streamlit as st
-import openai
-openai.api_key =  st.secrets["mykey"]
-
 import ast
 import pandas as pd
+import streamlit as st
 from openai.embeddings_utils import cosine_similarity
+import openai
+
+# Set your OpenAI API key
+openai.api_key = st.secrets["mykey"] 
 
 df = pd.read_csv("qa_dataset_with_embeddings.csv")
 
 # Convert the string embeddings back to lists
 df['Question_Embedding'] = df['Question_Embedding'].apply(ast.literal_eval)
+
+def get_embedding(text, model="text-embedding-ada-002"):
+   return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
 
 def find_best_answer(user_question):
    # Get embedding for the user's question
@@ -23,7 +27,7 @@ def find_best_answer(user_question):
    max_similarity = df['Similarity'].max()
 
    # Set a similarity threshold to determine if a question is relevant enough
-   similarity_threshold = 0.6  # You can adjust this value
+   similarity_threshold = 0.75  # You can adjust this value
 
    if max_similarity >= similarity_threshold:
       best_answer = df.loc[most_similar_index, 'Answer']
@@ -32,13 +36,17 @@ def find_best_answer(user_question):
       return "I apologize, but I don't have information on that topic yet. Could you please ask other questions?"
 
 
-# Streamlit UI
-st.title("Smart FAQ Assistant")
+# Streamlit Interface
+st.title("Smart FAQ Assistant (Heart, Lung, Blood Health)")
 
-product_name = st.text_input("Please enter your question:", value="Type your question here")
+user_question = st.text_input("Ask a question","Who will have Cardiomyopathy?")
+search_button = st.button("Find Answer")
 
-
-if st.button("Find Answer"):
-    caption, image_description = generate_copy(answer)
-    st.subheader("Answer:")
-  
+if search_button:
+    if not user_question:
+        st.warning("Please enter a question.")
+    else:
+        with st.spinner("Searching for the best answer..."):  # Display a spinner while searching
+            answer = find_best_answer(user_question)
+            st.write("## Answer:")
+            st.write(answer)
